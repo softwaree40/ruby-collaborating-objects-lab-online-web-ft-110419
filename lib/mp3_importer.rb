6c958160b781e4103b95f5a7be94d371e0d6a542
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-28 22:11:20 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,16 @@
+require "pry"
 class MP3Importer
       attr_accessor:path
       
     def initialize(path)
         @path = path
+        #binding.pry
     end
-  
-   
-  
+    def files
+       Dir.entries(path).select {|entry| entry.include?(".mp3")}
+         
+    end
+    def import
+      files.each {|filename| Song.new_by_filename(filename)}
+    end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,16 @@ class Song
     def self.all
       @@all
     end
-   
+    def self.new_by_filename(file)
+      song_name = file.split(" - ")[1]
+      artist = file.split(" - ")[0]
+      song= self.new(song_name)
+      song.artist= artist
+      song.artist.name
+        
+    end
+    def artist_name
+      self.artist= Artist.find_or_create_by_name(name)
+      self.artist.add_song(self)
+    end
 end
